3d8f27f2b1ca99660ec5212344f413f18dfd5c41
Mapmaker <bra_shing@mail.ru>
2023-11-14 22:23:01 +0300

    Добавлены новости, актуализированы ссылки и фото. Созданы встраиваемые шаблоны login,registration,news_input. Добавлена страница аккаунт пользователя.

--- NewsStudyRostelecom/main/urls.py
+++ NewsStudyRostelecom/main/urls.py
@@ -5,4 +5,11 @@ from . import views
 urlpatterns = [
     path('', views.index, name='home'),
     path('news/', views.news, name='news'),
+    path('news_1/', views.news_1, name='news_1'),
+    path('news_2/', views.news_2, name='news_2'),
+    path('news_3/', views.news_3, name='news_3'),
+    path('news_4/', views.news_4, name='news_4'),
+    path('news_5/', views.news_5, name='news_5'),
+    path('news_6/', views.news_6, name='news_6'),
+    path('user_profile/', views.user_profile, name='user_profile'),
 ]
--- NewsStudyRostelecom/main/views.py
+++ NewsStudyRostelecom/main/views.py
@@ -7,6 +7,27 @@ def index(request):
 def news(request):
     return render(request, 'main/news.html')
 
+def news_1(request):
+    return render(request, 'main/news_1.html')
+
+def news_2(request):
+    return render(request, 'main/news_2.html')
+
+def news_3(request):
+    return render(request, 'main/news_3.html')
+
+def news_4(request):
+    return render(request, 'main/news_4.html')
+
+def news_5(request):
+    return render(request, 'main/news_5.html')
+
+def news_6(request):
+    return render(request, 'main/news_6.html')
+
+def user_profile(request):
+    return render(request, 'main/user_profile.html')
+
 def custom_404(request, exception):
     # return render(request, 'main/sidebar.html')
     return HttpResponse(f'Ой, какая жалость!:{exception}')
