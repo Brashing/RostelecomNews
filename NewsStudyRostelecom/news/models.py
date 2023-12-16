a6479c05627ff30822381ab019c3ad9ca64e4300
Mapmaker <bra_shing@mail.ru>
2023-12-16 23:27:58 +0300

    16.12.2023 Исправлена ошибка открытия модели Новости в панели администратора при удалении записи из модели Фото.

--- NewsStudyRostelecom/news/models.py
+++ NewsStudyRostelecom/news/models.py
@@ -52,7 +52,7 @@ class Article(models.Model):
 
     def image_tag(self):
         image = Image.objects.filter(article=self)
-        if image is not None:
+        if image:
             return mark_safe(f'<img src="{image[0].image.url}" height="50px" width="auto" />')
         else:
             return '(no image)'
@@ -81,7 +81,7 @@ class Subscriber(models.Model):
 class Image(models.Model):
     article = models.ForeignKey(Article, on_delete=models.CASCADE)
     title = models.CharField(max_length=50, blank=True)
-    image = models.ImageField(upload_to='news_images/')
+    image = models.ImageField(upload_to='news_images/', blank=True, null=True)
 
     def __str__(self):
         return self.title
